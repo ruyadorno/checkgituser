93fb338be1cbc4617ac794912ea95c50073de038
Ruy Adorno <ruyadorno@hotmail.com>
2014-03-13 16:43:14 -0400

    Added initial implementation for testing .git/config files

---
+++ checkgituser.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+
+dest_path = sys.argv[1]
+
+print 'Checking for .git/config files without an user email...\n'
+
+for path, dirs, files in os.walk(dest_path):
+    for filename in files:
+
+        if filename == 'config' and path.find('.git') > -1:
+            has_config = False
+            fullpath = os.path.join(path, filename)
+
+            with open(fullpath, 'r') as f:
+                for line in f:
+
+                    if line.find('email') > -1:
+                        has_config = True
+
+            if not has_config:
+                print fullpath
